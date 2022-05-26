c10d6080343d545db1d219c8846bb62ab7f95082
Marcos Nikel <marcosnikeldev@gmail.com>
2022-05-25 23:12:26 -0300

    new route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import userController from '../controller/userController.js';
 
 const router = express.Router();
 
 router
   .get('/users', (req, res) => {
     res.status(200).send('batata');
-  });
+  })
+
+  .post('/users', userController.getImc);
 
 export default router;
