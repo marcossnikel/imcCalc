02fbfe5970505c672b4935c4e594806843d41bb3
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2022-06-01 20:31:16 -0300

    feat(jest): adiciona nos pacotes configuracoes de jest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,9 +2,11 @@ module.exports = {
   env: {
     es2021: true,
     node: true,
+    jest: true
   },
   extends: [
     'airbnb-base',
+    'jest',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
