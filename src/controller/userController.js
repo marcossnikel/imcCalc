314ee7e3afbfc00c734fa7501a6ec3c8c597f99a
Marcos Nikel <marcosnikeldev@gmail.com>
2022-06-09 09:48:25 -0300

    user controller

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -3,55 +3,31 @@ import user from '../models/User.js';
 // Método para listar os usuarios.
 class UserController {
   static getUsers = (req, res) => {
-    user.find((err, users) => {
-      if (err) {
-        res.status(400).send({ message: `${err.message} - User not Found` });
-      }
+    try{
+      user.findAll((users)=>{
+        return res.json(users);
+      }) 
+      
+    }catch(error){
+      return res.json(error.message);
+    }
 
-      res.json(users);
-    });
   };
 
-  // IMC Formula
 
-  static getImc = (req, res) => {
-    const usuario = {
-      name: req.body.name,
-      weigth: req.body.weigth,
-      heigth: req.body.heigth,
-      age: req.body.age,
-    };
-
-    const { weigth } = usuario;
-    const { heigth } = usuario; // Meters 176 = 1.76
-
-    const imc = weigth / heigth ** 2;
-
-    res.json({
-      imc,
-    });
-  };
-
-  // Harris Benedict Formula (Male)
-
-  static harrisBenedictFormula = (req, res) => {
-    const usuario = {
-      name: req.body.name,
-      weigth: req.body.weigth,
-      heigth: req.body.heigth,
-      age: req.body.age,
-    };
-
-    const { weigth } = usuario;
-    const { heigth } = usuario; // Centimeters 1.76 = 176
-    const { age } = usuario;
-
-    const harris = 66 + (13.8 * weigth) + (5 * heigth) - (6.8 * age);
-
-    res.json({
-      harris,
-    });
-  };
+  static getUserId = (req, res) => {
+    const { id } = req.params
+    try {
+      const oneUser = user.findOne({
+        where: {
+          id: Number(id)
+        },
+      })
+      return res.json(oneUser)
+    } catch (error) {
+      res.json(error.message);
+    }
+  }
 }
 
 export default UserController;
