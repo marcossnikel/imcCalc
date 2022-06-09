3814ca4d197f816a6c325101268c4df4ecee1857
Marcos Nikel <marcosnikeldev@gmail.com>
2022-06-09 09:47:54 -0300

    validacao numero teste

--- src/tests/utils/formulas/imc.test.js
+++ src/tests/utils/formulas/imc.test.js
@@ -1,8 +1,12 @@
+import res from 'express/lib/response';
 import imc from '../../../utils/formulas/imc';
 
 describe('Testing IMC formula', () => {
   it('should calculate imc', () => {
     const result = imc(1.70, 65);
     expect(result).toBe(22.49);
+    if(result != Number){
+      console.log('Input should be a number')
+    }
   });
 });
