5f18557b9369f4a52dfa6446cc25394629b059be
Marcos Nikel <marcosnikeldev@gmail.com>
2022-06-08 21:30:26 -0300

    harris formula

---
+++ src/utils/formulas/harrisMale.js
@@ -0,0 +1,9 @@
+const harris = (weigth, heigth, age,) => {
+
+  const maleHarris = 66 + (13.8 * weigth) + (5 * heigth) - (6.8 * age);
+
+  return Math.floor(maleHarris * 100) / 100
+}
+
+
+export default harris;
