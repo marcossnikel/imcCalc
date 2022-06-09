fc73f4701b70dc58da296c3906fb26c8b4dbc479
Marcos Nikel <marcosnikeldev@gmail.com>
2022-06-08 21:30:11 -0300

    adiciona test formula harris

---
+++ src/tests/utils/formulas/harrisMale.test.js
@@ -0,0 +1,8 @@
+import harris from '../../../utils/formulas/harrisMale';
+
+describe('Testing Harris Formula', () => {
+  it('Should calculate Harris Benedict (MALE)',() => {
+    const calories = harris(65,175,20);
+    expect(calories).toBe(1702);
+  });
+});
