9583a81bac908ac03d735bd25daa9c5bad521d70
Marcos Nikel <marcosnikeldev@gmail.com>
2022-06-09 09:47:40 -0300

    rota formula novo

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import UserController from '../controller/userController.js';
+import FormulaController from '../controller/formulaController.js';
 import user from '../models/User.js';
 
 const router = express.Router();
 
 router
-  .get('/users', (req, res) => {
-    res.status(200).send('batata');
-  })
-
-  .post('/users', UserController.getImc)
-  .post('/users/harris', UserController.harrisBenedictFormula);
+  .get('/users',UserController.getUsers)
+  .get('/users/:id',UserController.getUserId)
+  .post('/formulas/imc', FormulaController.getImc)
+  .post('/formulas/harris', FormulaController.maleHarris)
 
 export default router;
